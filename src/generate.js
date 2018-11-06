4a499fa8f47a2bbdfbee3e7ab68e1ffdce8673dd
Hitoshi Honma <homma_hitoshi_gn@cyberagent.co.jp>
2018-11-06 15:10:13 +0900

    Replace Axios to an ajax object

--- src/generate.js
+++ src/generate.js
@@ -1,13 +1,14 @@
 export function generateCode(actionParams) {
-  return `import axios from 'axios'
-
-export const types = [
+  return `export const types = [
 ${actionParams.map(({ mutationType })=>`  '${mutationType}',`).join('\n')}
 ].reduce(function(obj, val){ return Object.assign(obj, {[val]: val}) }, {})
 
+export let ajax = {}
+export function init (a) { ajax = a }
+
 ${actionParams.map(({ method, url, hasQuery, hasBody, actionName, mutationType, args, options })=>(
 `export function ${actionName} (${args}) {
-  return axios.${method}(\`${url}\`${hasBody ? ', body' : ''}${options ? `, ${options}` : ''})
+  return ajax.${method}(\`${url}\`${hasBody ? ', body' : ''}${options ? `, ${options}` : ''})
     .then(function(res){
       context.commit(types.${mutationType}, res.data)
       return res.data
